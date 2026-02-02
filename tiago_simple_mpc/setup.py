9bbf0f0d5d766d7c41297bac0c09f89c76a336da
clementPene <pene.clement@gmail.com>
2026-02-02 15:55:44 +0100

    chore(package): update setup files

--- tiago_simple_mpc/setup.py
+++ tiago_simple_mpc/setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+import os
 from setuptools import find_packages, setup
 
 package_name = 'tiago_simple_mpc'
@@ -10,13 +12,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # copy yaml files to share/tiago_simple_mpc/config
+        (os.path.join('share', package_name, 'config'),
+         glob('tiago_simple_mpc/config/*.yaml')),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'meshcat',  # 🎨 Ajouté pour visualisation
+    ],
     zip_safe=True,
     maintainer='cpene',
     maintainer_email='pene.clement@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Simple MPC controller for Tiago using Crocoddyl',
+    license='Apache-2.0',  # ou ta licence
     extras_require={
         'test': [
             'pytest',
@@ -24,9 +32,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'simple_mpc_node = tiago_simple_mpc.simple_mpc_node:main',
-            'cartesian_target_mpc_node = tiago_simple_mpc.cartesian_target_mpc_node:main',
-            'patch_urdf_effort = tiago_simple_mpc.tools.patch_urdf_effort:main',
+            'cartesian_target_mpc_node = tiago_simple_mpc.nodes.cartesian_target_mpc_node:main',
+            'test_ocp_reaching = tiago_simple_mpc.tests.test_ocp_reaching:main',
         ],
     },
 )
